6da3949cfc8b37c4e68f5277946a041d64e23e4b
mirek0707 <80774381+mirek0707@users.noreply.github.com>
2021-10-06 21:54:59 +0200

    Add files via upload

---
+++ lab1/Test.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class Test{
+    public static void main(String[] args)
+    {
+        Scanner scanner1 = new Scanner(System.in);
+        System.out.print("Podaj promien: ");
+        double r=scanner1.nextDouble();
+        System.out.println("Pole powierzchni: "+Math.round(Math.PI*Math.pow(r,2)));
+        System.out.println("Obwod kola: "+Math.round(2*Math.PI*r));
+        
+    }
+}
