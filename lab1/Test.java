39a1d870ca2c18dc98722b52d8b3d34b176a5c9b
mirek0707 <80774381+mirek0707@users.noreply.github.com>
2021-10-06 21:55:20 +0200

    Update Test.java

--- lab1/Test.java
+++ lab1/Test.java
@@ -5,8 +5,8 @@ public class Test{
         Scanner scanner1 = new Scanner(System.in);
         System.out.print("Podaj promien: ");
         double r=scanner1.nextDouble();
-        System.out.println("Pole powierzchni: "+Math.round(Math.PI*Math.pow(r,2)));
-        System.out.println("Obwod kola: "+Math.round(2*Math.PI*r));
+        System.out.println("Pole powierzchni: "+(Math.PI*Math.pow(r,2)));
+        System.out.println("Obwod kola: "+(2*Math.PI*r));
         
     }
 }
